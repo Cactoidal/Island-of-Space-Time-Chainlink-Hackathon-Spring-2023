187f3de6b52d11d00c280f44e787b83d2818c401
Cactus <115384394+Cactoidal@users.noreply.github.com>
2023-06-04 23:27:24 -0400

    Update Summoner.gd

--- godot/Summoner.gd
+++ godot/Summoner.gd
@@ -1,43 +1,92 @@
-func accept():
-	if User.user_balance == "0":
-		$PauseMenu/Prompt/Proceed.text = "Need MATIC! Press ESC"
-		return
+var http_request_delete
+func summon():
+	var http_request = HTTPRequest.new()
+	get_parent().get_node("Summoning").add_child(http_request)
+	http_request_delete = http_request
+	http_request.connect("request_completed", self, "upload_image")
+
+	var body = JSON.print({"prompt": User.ai_query, "n": 1, "size": "256x256", "response_format": "b64_json"})
+		
+	var error = http_request.request("https://api.openai.com/v1/images/generations", 
+	["Content-Type: application/json", "authorization: Bearer " + User.open_ai_key], 
+	true, 
+	HTTPClient.METHOD_POST, 
+	body)
+	
+	get_parent().get_node("DataEntry/Log").text += "\nSummoning..."
+	get_parent().get_node("DataEntry").reset_log_fade()
+
+func upload_image(result, response_code, headers, body):
+	
+	if response_code == 200:
+	
+		get_parent().get_node("DataEntry/Log").text += "\nSummoning Returned"
+		get_parent().get_node("DataEntry").reset_log_fade()
+		
+		var image_string = parse_json(body.get_string_from_ascii())["data"][0]["b64_json"]
+		
+		if User.refresh_token == "":
+			var file = File.new()
+			file.open("user://keystore", File.READ)
+			var content = file.get_buffer(32)
+			KeyGen.check_operational(content, get_parent().get_node("DataEntry"))
+			if User.relay_operational == true:
+				KeyGen.request_token(content, get_parent().get_node("DataEntry"))
+			file.close()
+		
+		pending_hash = generate_hash()
+		
+		var http_request = HTTPRequest.new()
+		get_parent().get_node("Uploading").add_child(http_request)
+		
+		http_request.connect("request_completed", self, "image_uploaded")
+
+		var request_body = JSON.print({"resourceId": "IMAGINARY.CREATURES", 
+		"sqlText": "INSERT INTO IMAGINARY.CREATURES(HASH, ID, IMAGE) VALUES ('" + pending_hash + "', '" + str(pending_creature_id) + "', '" + image_string + "')"})
+		
+		var error = http_request.request("https://hackathon.spaceandtime.dev/v1/sql/dml", 
+		["accept: application/json", "authorization: Bearer " + User.auth_token, "biscuit: " + User.biscuit, "content-type: application/json"], 
+		true, 
+		HTTPClient.METHOD_POST, 
+		request_body)
+		
+		http_request_delete.queue_free()
+		http_request_delete = http_request
+		
+	else:
+		get_parent().get_node("DataEntry/Log").text += "\nSummoning Failed!\nInvalid OpenAI Key!"
+		get_parent().get_node("DataEntry").reset_log_fade()
+		summoning_ongoing = false
+	
+		http_request_delete.queue_free()
+
+
+func image_uploaded(result, response_code, headers, body):
+	if response_code == 200:
+		get_parent().get_node("DataEntry/Log").text += "\nSummoning Uploaded"
+		get_parent().get_node("DataEntry").reset_log_fade()
+		
+		initialize_creature()
+	else:
+		get_parent().get_node("DataEntry/Log").text += "\nSummoning Failed!\nInvalid SxT Token!"
+		get_parent().get_node("DataEntry").reset_log_fade()
+		summoning_ongoing = false
+	http_request_delete.queue_free()
+	http_request_delete = null
+	
+
+func initialize_creature():
+	randomize()
+	var demeanor = randi()%2+1;
+	randomize()
+	var location = randi()%3+1;
 	var file = File.new()
 	file.open("user://keystore", File.READ)
 	var content = file.get_buffer(32)
-	KeyGen.get_creature(content, self)
-	KeyGen.do_vrf(content)
+	KeyGen.initialize_creature(content, pending_creature_id, pending_hash, location, demeanor)
 	file.close()
-	$Log.text += "\nCalling Chainlink VRF..."
-	reset_log_fade()
-	$Log/Eprompt.visible = true
-	$PauseMenu/CopyAddress.visible = true
-	$PauseMenu/FaucetLink.visible = true
-	$PauseMenu/Return.visible = true
-	$PauseMenu.visible = false
-	$PauseMenu/Prompt.text = ""
-	$PauseMenu/Prompt.visible = false
-	get_parent().get_node("Player").summoning_ongoing = true
-	get_parent().get_node("Player").summoning = false
-	get_parent().get_node("Player").menu_open = false
-	get_parent().get_node("Player").mouse_captured = true
-	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
-	start_check = true
-
-func check_query():
-	$Log.text += "\nAwaiting emanation..."
-	reset_log_fade()
-	var file = File.new()
-	file.open("user://keystore", File.READ)
-	var content = file.get_buffer(32)
-	KeyGen.check_query_return(content, pending_creature_id, self)
-	file.close()
-
-var returned = "false"
-func query_checked(var check_status):
-	returned = check_status
-	print(returned)
-	if returned == "true":
-		start_check = false
-		check_timer = 7
-		get_query = true
+	get_parent().get_node("DataEntry/Log").text += "\nCreature Initialized"
+	get_parent().get_node("DataEntry").reset_log_fade()
+	summoning_ongoing = false
+	recheck_menu = true
+	User.get_creatures()
